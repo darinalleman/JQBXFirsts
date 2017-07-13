a6118e7dc050d21a4cd50bb01daccc3a4c348e52
programmer <programmer@localhost.localdomain>
2017-07-13 14:58:37 -0500

    loading playlists now

--- src/app/playlists/playlists.component.ts
+++ src/app/playlists/playlists.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {SpotifyService} from "../shared/spotify/angular2-spotify";
+import * as _ from "lodash";
 
 @Component({
   selector: 'app-playlists',
@@ -8,21 +9,37 @@ import {SpotifyService} from "../shared/spotify/angular2-spotify";
 })
 export class PlaylistsComponent implements OnInit {
   public user: any;
-  public playlists: any[];
+  public playlists: any;
+  public options: any;
 
   constructor(public spotifyService: SpotifyService) {
-    this.user = JSON.parse(localStorage.getItem('user'));
-    this.getUserPlaylists();
   }
 
   ngOnInit() {
+      this.user = JSON.parse(localStorage.getItem('user'));
+      this.getUserPlaylists();
+
   }
 
-  getUserPlaylists() {
-    this.spotifyService.getUserPlaylists(this.user.id).subscribe(
+  showPlaylist(playlist){
+    console.log(playlist.id, playlist.owner.id);
+    this.spotifyService.getPlaylist(playlist.owner.id, playlist.id).subscribe(
       data => {
-        this.playlists = data.items;
-        console.log(this.playlists);
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  };
+
+  getUserPlaylists() {
+    this.options = {
+      limit: 40
+    };
+    this.spotifyService.getUserPlaylists(this.user.id, this.options).subscribe(
+      data => {
+          this.playlists = data.items;
       },
       error => {
         console.log(error);
