7e7ab1f024f32a7ecd5cb0a349403741f5c128c0
programmer <programmer@localhost.localdomain>
2017-07-31 15:31:10 -0500

    ability to save and unsave albums added | related artist card style changes

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as moment from 'moment';
 import * as _ from "lodash";
+import {SpotifyService} from "../shared/spotify/angular2-spotify";
 
 @Component({
   selector: 'app-album',
@@ -8,18 +9,56 @@ import * as _ from "lodash";
   styleUrls: ['./album.component.scss']
 })
 export class AlbumComponent implements OnInit {
-  public album : any;
-  constructor() { }
+  public album: any;
+  private saved: boolean;
+
+  constructor(public spotifyService: SpotifyService) {
+  }
 
   ngOnInit() {
     this.loadAlbum();
+    this.checkAlbum();
   }
 
   loadAlbum() {
     this.album = JSON.parse(localStorage.getItem('album'));
     _.each(this.album.album.tracks.items, track => {
-        track.duration_ms = moment(track.duration_ms).format('m:ss');
+      track.duration_ms = moment(track.duration_ms).format('m:ss');
     });
+  }
+
+  checkAlbum() {
+    this.album = JSON.parse(localStorage.getItem('album'));
+    this.spotifyService.userAlbumsContains(this.album.album.id).subscribe(
+      data => {
+        this.saved = data[0];
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+
+  removeAlbum(album) {
+    this.spotifyService.removeUserAlbums(album.id).subscribe(
+      () => {
+        this.checkAlbum();
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+
+  saveAlbum(album) {
+    this.spotifyService.saveUserAlbums(album.id).subscribe(
+      () => {
+        this.checkAlbum();
+      },
+      error => {
+        console.log(error);
+      }
+    )
   }
 
 }
