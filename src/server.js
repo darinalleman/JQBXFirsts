2db5773466014b412954da171203ee8dc823fa31
cmuzzin <cmuzzin@uark.edu>
2017-08-18 21:23:55 -0500

    first deploy

--- src/server.js
+++ src/server.js
@@ -4,15 +4,10 @@
 // server.js
 const express = require('express');
 const app = express();
-const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 
-
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
 // Heroku port
 app.listen(process.env.PORT || 8080);
