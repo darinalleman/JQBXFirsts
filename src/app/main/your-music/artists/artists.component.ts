98c2da8c916654bcc38c01e8ed9e67ff1d37082e
ddsfoundation3 (JAZZ) <fnd_devs@dillards.com>
2017-09-08 16:09:06 -0500

    header styles changes to user, profile, artist component. New app background image

--- src/app/main/your-music/artists/artists.component.ts
+++ src/app/main/your-music/artists/artists.component.ts
@@ -1,43 +1,64 @@
-import {Component, OnInit} from '@angular/core';
-import {SpotifyService} from '../../../shared/spotify/angular2-spotify';
-import {Router} from '@angular/router';
-import {LoadArtistService} from './artist/load-artist.service';
+import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../../shared/spotify/angular2-spotify';
+import { Router } from '@angular/router';
+import { LoadArtistService } from './artist/load-artist.service';
+import * as _ from 'lodash';
 
 @Component({
-  selector: 'app-artists',
-  templateUrl: './artists.component.html',
-  styleUrls: ['./artists.component.scss']
+    selector: 'app-artists',
+    templateUrl: './artists.component.html',
+    styleUrls: ['./artists.component.scss']
 })
 export class ArtistsComponent implements OnInit {
-  public artists: any;
-  public options: any;
-  private type: string;
+    cursors: any;
+    artists: any;
+    options: any;
+    type: string;
+    totalArtists: number;
 
-  constructor(public spotifyService: SpotifyService, public router: Router, private loadArtistService: LoadArtistService) {
-  }
+    constructor(public spotifyService: SpotifyService, public router: Router, private loadArtistService: LoadArtistService) {
+    }
 
-  ngOnInit() {
-    this.getSavedArtists();
-  }
+    ngOnInit() {
+        this.getSavedArtists();
+    }
 
-  getSavedArtists() {
-    this.options = {
-      limit: 50
+    getSavedArtists() {
+        this.options = {
+            limit: 50
+        };
+        this.type = 'artist';
+        this.spotifyService.getFollowedArtists(this.type, this.options).subscribe(
+            data => {
+                this.cursors = data.artists.cursors;
+                this.artists = data.artists.items;
+                this.totalArtists = data.artists.total;
+            },
+            error => {
+                console.log(error);
+            }
+        )
+    }
+
+    goToArtist(artist) {
+        this.loadArtistService.currentArtist.next(artist);
+        this.router.navigate(['main/artist'])
     };
-    this.type = 'artist';
-    this.spotifyService.getFollowedArtists(this.type, this.options).subscribe(
-      data => {
-        this.artists = data.artists.items;
-      },
-      error => {
-        console.log(error);
-      }
-    )
-  }
 
-  goToArtist(artist) {
-    this.loadArtistService.currentArtist.next(artist);
-    this.router.navigate(['main/artist'])
-  };
+    loadMoreArtists() {
+        this.options = {
+            limit: 20,
+            after: this.cursors.after
+        };
+        this.spotifyService.getFollowedArtists(this.type, this.options).subscribe(
+            data => {
+                this.artists = _.concat(this.artists, data.artists.items);
+                document.getElementById('loadMoreArtists').blur();
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    };
 
 }
