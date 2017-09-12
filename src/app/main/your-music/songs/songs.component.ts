6e786b44243156d7d7133d7f344f0d0c88354a52
cmuzzin <cmuzzin@uark.edu>
2017-09-11 23:17:56 -0500

    more areas to add song to a playlist, buttons on playlist and album comp for -- were links

--- src/app/main/your-music/songs/songs.component.ts
+++ src/app/main/your-music/songs/songs.component.ts
@@ -1,106 +1,120 @@
-import { Component, OnInit } from '@angular/core';
-import { SpotifyService } from '../../../shared/spotify/angular2-spotify';
+import {Component, OnInit} from '@angular/core';
+import {SpotifyService} from '../../../shared/spotify/angular2-spotify';
 import * as moment from 'moment';
 import * as _ from 'lodash';
-import { ActiveSongService } from '../../music-player/active-song.service';
-import { NavigationService } from '../../../shared/navigation/navigation.service';
+import {ActiveSongService} from '../../music-player/active-song.service';
+import {NavigationService} from '../../../shared/navigation/navigation.service';
+import {AddSongToPlaylistService} from '../../modals/add-to-playlist-modal/add-song-to-playlist.service';
 
 @Component({
-    selector: 'app-songs',
-    templateUrl: './songs.component.html',
-    styleUrls: ['./songs.component.scss']
+  selector: 'app-songs',
+  templateUrl: './songs.component.html',
+  styleUrls: ['./songs.component.scss']
 })
 export class SongsComponent implements OnInit {
-    public tracks: any;
-    public options: any;
-    public offset: any;
-    public totalTracks: any;
-    public album: any;
-    public playObject: any;
-    public selectedRow: any;
+  trackToAdd: any;
+  tracks: any;
+  options: any;
+  offset: any;
+  totalTracks: any;
+  album: any;
+  playObject: any;
+  selectedRow: any;
 
-    constructor(private spotifyService: SpotifyService,
-                private activeSongService: ActiveSongService,
-                private navigationService: NavigationService) {
-    }
+  constructor(private spotifyService: SpotifyService,
+              private activeSongService: ActiveSongService,
+              private navigationService: NavigationService,
+              private addSongToPlaylistService: AddSongToPlaylistService) {
+  }
 
-    ngOnInit() {
-        this.offset = 0;
-        this.getSavedTracks();
-    }
+  ngOnInit() {
+    this.offset = 0;
+    this.getSavedTracks();
+    this.addSongToPlaylistService.songToAddToPlaylist.subscribe(
+      songBeingAdded => {
+        this.trackToAdd = songBeingAdded;
+      }
+    )
+  }
 
-    getSavedTracks() {
-        this.options = {
-            limit: 50
+  getSavedTracks() {
+    this.options = {
+      limit: 50
+    };
+    this.spotifyService.getSavedUserTracks(this.options).subscribe(
+      data => {
+        this.tracks = data.items;
+        this.totalTracks = data.total;
+        _.each(this.tracks, (track: any) => {
+          track.track.duration_ms = moment(track.track.duration_ms).format('m:ss')
+        })
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+
+  loadMoreTracks() {
+    this.offset = this.offset + 50;
+    this.options = {
+      limit: 50,
+      offset: this.offset
+    };
+    this.spotifyService.getSavedUserTracks(this.options).subscribe(
+      data => {
+        _.each(data.items, (track: any) => {
+          track.track.duration_ms = moment(track.track.duration_ms).format('m:ss');
+        });
+        this.tracks = _.concat(this.tracks, data.items);
+        document.getElementById('loadMoreSongsButton').blur();
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  };
+
+  goToArtist(artist) {
+    this.navigationService.goToArtist(artist);
+  };
+
+  goToAlbum(album) {
+    this.spotifyService.getAlbum(album.id).subscribe(
+      data => {
+        this.album = {
+          album: data
         };
-        this.spotifyService.getSavedUserTracks(this.options).subscribe(
-            data => {
-                this.tracks = data.items;
-                this.totalTracks = data.total;
-                _.each(this.tracks, (track: any) => {
-                    track.track.duration_ms = moment(track.track.duration_ms).format('m:ss')
-                })
-            },
-            error => {
-                console.log(error);
-            }
-        )
-    }
+        this.navigationService.goToAlbum(this.album);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  };
 
-    loadMoreTracks() {
-        this.offset = this.offset + 50;
-        this.options = {
-            limit: 50,
-            offset: this.offset
-        };
-        this.spotifyService.getSavedUserTracks(this.options).subscribe(
-            data => {
-                _.each(data.items, (track: any) => {
-                    track.track.duration_ms = moment(track.track.duration_ms).format('m:ss');
-                });
-                this.tracks = _.concat(this.tracks, data.items);
-                document.getElementById('loadMoreSongsButton').blur();
-            },
-            error => {
-                console.log(error);
-            }
-        )
+  startSong(songUri) {
+    this.playObject = {
+      'uris': [songUri]
     };
+    this.spotifyService.startResumePlayer(this.playObject).subscribe(
+      () => {
+      },
+      error => {
+        console.log(error);
+      }
+    )
+  };
 
-    goToArtist(artist) {
-        this.navigationService.goToArtist(artist);
-    };
+  toggleAddToPlaylistModal(track) {
+    this.addSongToPlaylistService.songToAddToPlaylist.next(track);
+    const modal = document.getElementById('addToPlaylistModal');
+    modal.classList.toggle('is-active');
+  };
 
-    goToAlbum(album) {
-        this.spotifyService.getAlbum(album.id).subscribe(
-            data => {
-                this.album = {
-                    album: data
-                };
-                this.navigationService.goToAlbum(this.album);
-            },
-            error => {
-                console.log(error);
-            }
-        );
-    };
-
-    startSong(songUri) {
-        this.playObject = {
-            'uris': [songUri]
-        };
-        this.spotifyService.startResumePlayer(this.playObject).subscribe(
-            () => {
-            },
-            error => {
-                console.log(error);
-            }
-        )
-    };
-
-    setClickedRow(index, track) {
-        this.selectedRow = index;
-        this.startSong(track.track.uri);
-        this.activeSongService.currentSong.next(track.track);
-    };
+  setClickedRow(index, track) {
+    this.selectedRow = index;
+    this.startSong(track.track.uri);
+    this.activeSongService.currentSong.next(track.track);
+  };
 }
