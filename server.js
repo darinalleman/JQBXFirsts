debd9d14e5dd8ca5cede80a62e484f255242dd0d
Darin Alleman <darin.alleman@gmail.com>
2018-09-14 19:53:11 -0400

    Bug testing

--- server.js
+++ server.js
@@ -8,19 +8,18 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/callback', function (req, res) {
-    console.log('We got here');
     console.log(res);
-    var hash = window.location.hash;
-    if (hash) {
-      if (window.location.search.substring(1).indexOf('error') !== -1) {
-        // login failure
-        window.close();
-      } else if (hash) {
-        // login success
-        var token = window.location.hash.split('&')[0].split('=')[1];
-        localStorage.setItem('angular2-spotify-token', token);
-      }
-    } else {
-      window.close();
-    }
-})
+    // var hash = window.location.hash;
+    // if (hash) {
+    //   if (window.location.search.substring(1).indexOf('error') !== -1) {
+    //     // login failure
+    //     window.close();
+    //   } else if (hash) {
+    //     // login success
+    //     var token = window.location.hash.split('&')[0].split('=')[1];
+    //     localStorage.setItem('angular2-spotify-token', token);
+    //   }
+    // } else {
+    //   window.close();
+    // }
+});
