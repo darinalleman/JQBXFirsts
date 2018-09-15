0365b3f5270025d83527bd933c80b074ee0c94a0
Darin Alleman <darin.alleman@gmail.com>
2018-09-14 21:31:37 -0400

    Here we go!

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
 app.get('*', function(req, res) {
-  res.sendfile('./app/index.html')
+  res.sendfile('./index.html')
 })
