512dd4dfa1bf44dad83c843df64c593a52e4da72
Darin Alleman <darin.alleman@gmail.com>
2018-09-14 22:13:31 -0400

    I think I got it

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
 app.get('*', function(req, res) {
-  res.sendfile('./src/index.html', {'root':'./'});
+  res.sendfile('./src/app/callback/callback.component.html', {'root':'./'});
 });
