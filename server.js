a3b947707e7f34cefeb28553fd5f0e255db3a6a3
Darin Alleman <darin.alleman@gmail.com>
2018-09-14 22:04:13 -0400

    Change root

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
 app.get('*', function(req, res) {
-  res.sendfile('../index.html');
-})
+  res.sendfile('./src/index.html', {'root':'/'});
+});
