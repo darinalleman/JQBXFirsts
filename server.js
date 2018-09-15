9f7b9fd838cbdf8cf1e89f4130eaad00ad423c4c
Darin Alleman <darin.alleman@gmail.com>
2018-09-14 21:27:25 -0400

    Attempt #345

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
+
+app.get('*', function(req, res) {
+  res.sendfile('./app/index.html')
+})
